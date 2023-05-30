22cb4e91c8c926c68ecbd35d4b3ab54a7ab585a6
RayShawn Tyler <rtyler@appdev.yearup.org>
2023-05-30 13:30:58 -0400

    Chips menu

--- src/main/java/com/deli/Chips.java
+++ src/main/java/com/deli/Chips.java
@@ -1,4 +1,44 @@
 package com.deli;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Chips {
+    private List<String> availableChips;
+
+    public Chips() {
+        availableChips = new ArrayList<>();
+        availableChips.add("Regular");
+        availableChips.add("Barbecue");
+        availableChips.add("Sour Cream and Onion");
+        availableChips.add("Salt and Vinegar");
+    }
+
+    public void displayAvailableChips() {
+        System.out.println("Available Chips:");
+        for (int i = 0; i < availableChips.size(); i++) {
+            System.out.println((i + 1) + ". " + availableChips.get(i));
+        }
+    }
+
+    public String getCustomerChips() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of your chosen chip: ");
+        int choice = scanner.nextInt();
+
+        if (choice >= 1 && choice <= availableChips.size()) {
+            return availableChips.get(choice - 1);
+        } else {
+            System.out.println("Invalid choice. Please try again.");
+            return getCustomerChips();
+        }
+    }
+
+    public static void main(String[] args) {
+        Chips chips = new Chips();
+        chips.displayAvailableChips();
+        String customerChoice = chips.getCustomerChips();
+        System.out.println("You selected: " + customerChoice + " chip.");
+    }
 }
--- src/main/java/com/deli/Order.java
+++ src/main/java/com/deli/Order.java
@@ -1,39 +1,39 @@
 package com.deli;
 
-class Order {
-
-    private String customerName;
-    private List<Item> items;
-    private double totalPrice;
-    private static int orderNumber = 1;
-
-    public Order(String customerName) {
-        this.customerName = customerName;
-        this.items = new ArrayList<>();
-        this.totalPrice = 0;
-        this.orderNumber = orderNumber++;
-    }
-
-    public void addItem(Item item) {
-        this.items.add(item);
-        this.totalPrice += item.getPrice();
-    }
-
-    public void removeItem(Item item) {
-        this.items.remove(item);
-        this.totalPrice -= item.getPrice();
-    }
-
-    public double getTotalPrice() {
-        return this.totalPrice;
-    }
-
-    public String toString() {
-        return "Order{" +
-                "customerName='" + customerName + '\'' +
-                ", items=" + items +
-                ", totalPrice=" + totalPrice +
-                ", orderNumber=" + orderNumber +
-                '}';
-    }
-}
+//class Order {
+//
+//    private String customerName;
+//    private List<Item> items;
+//    private double totalPrice;
+//    private static int orderNumber = 1;
+//
+//    public Order(String customerName) {
+//        this.customerName = customerName;
+//        this.items = new ArrayList<>();
+//        this.totalPrice = 0;
+//        this.orderNumber = orderNumber++;
+//    }
+//
+//    public void addItem(Item item) {
+//        this.items.add(item);
+//        this.totalPrice += item.getPrice();
+//    }
+//
+//    public void removeItem(Item item) {
+//        this.items.remove(item);
+//        this.totalPrice -= item.getPrice();
+//    }
+//
+//    public double getTotalPrice() {
+//        return this.totalPrice;
+//    }
+//
+//    public String toString() {
+//        return "Order{" +
+//                "customerName='" + customerName + '\'' +
+//                ", items=" + items +
+//                ", totalPrice=" + totalPrice +
+//                ", orderNumber=" + orderNumber +
+//                '}';
+//    }
+//}
