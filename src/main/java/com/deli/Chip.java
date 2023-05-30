04810f61b046f37d36d4ddb05921e19ff5007203
Demetrius <Dmayfield@appdev.yearup.or>
2023-05-30 10:25:59 -0400

    added Chips

---
+++ src/main/java/com/deli/Chip.java
@@ -0,0 +1,7 @@
+package com.deli;
+
+public class Chip {
+    private String chipType;
+    private float chipPrice;
+
+}
---
+++ src/main/java/com/deli/Drink.java
@@ -0,0 +1,14 @@
+package com.deli;
+
+public class Drink {
+    private String drinkFlavor;
+    private int drinkSize;
+    private float chipPrice;
+
+    public Drink(String drinkFlavor, int drinkSize, float chipPrice) {
+        this.drinkFlavor = drinkFlavor;
+        this.drinkSize = drinkSize;
+        this.chipPrice = chipPrice;
+    }
+
+}
