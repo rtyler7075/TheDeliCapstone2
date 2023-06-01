3e22c896819c8fc299b66e14f7b960cca11f40b6
mtoor286 <mtoor@appdev.yearup.org>
2023-06-01 10:26:28 -0400

    90% done

--- src/main/java/com/deli/HomeScreen.java
+++ src/main/java/com/deli/HomeScreen.java
@@ -1,5 +1,8 @@
 package com.deli;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class HomeScreen {
@@ -144,21 +147,20 @@ public class HomeScreen {
 
         int sandwichSize = scanner.nextInt();
 
-        switch (sandwichSize) {
-            case 1:
-                System.out.println("You have selected a small sandwich.");
-                break;
-            case 2:
-                System.out.println("You have selected a medium sandwich.");
-                break;
-            case 3:
-                System.out.println("You have selected a large sandwich.");
-                break;
-            default:
-                System.out.println("Invalid input. Please try again.");
-                break;
-        }
-
+            switch (sandwichSize) {
+                case 1:
+                    System.out.println("You have selected a small sandwich.");
+                    break;
+                case 2:
+                    System.out.println("You have selected a medium sandwich.");
+                    break;
+                case 3:
+                    System.out.println("You have selected a large sandwich.");
+                    break;
+                default:
+                    System.out.println("Invalid input. Please try again.");
+                    break;
+            }
     }
 
     private static void toastingBread() {
@@ -447,6 +449,77 @@ public class HomeScreen {
     }
 
     private static void checkout() {
+        // Calculate total price
+        double sandwichPrice = 5.00;
+        double chipsPrice = 2.00;
+        double drinkPrice = 1.00;
+        double totalPrice = sandwichPrice + chipsPrice + drinkPrice;
+
+        // Order Details
+        System.out.println("Order Details:");
+        System.out.println("Bread: ");
+        String bread = scanner.nextLine();
+        System.out.println("Size: ");
+        String size = scanner.nextLine();
+        System.out.println("Chips: ");
+        String chips = scanner.nextLine();
+        System.out.println("Drink: ");
+        String drink = scanner.nextLine();
+
+        // Confirm or Cancel order
+        System.out.println("Select an option:");
+        System.out.println("1. Confirm order and create receipt");
+        System.out.println("2. Cancel order and go back to the home screen");
+        int optionChoice = scanner.nextInt();
+
+        if (optionChoice == 1) {
+
+            // Create a new object of the Checkout class.
+            Checkout checkout = new Checkout();
+
+            // Call the setBread(), setSize(), setChips(), and setDrink() methods of the Checkout object, passing in the appropriate values.
+            checkout.setBread(bread);
+            checkout.setSize(size);
+            checkout.setChips(chips);
+            checkout.setDrink(drink);
+
+            // Call the getTotalPrice() method of the Checkout object, and store the return value in a variable.
+            double totalPriceFromCheckout = checkout.getTotalPrice();
+
+            // Print the order details to the terminal.
+            System.out.println("Order Details:");
+            System.out.println("Bread: " + checkout.bread);
+            System.out.println("Size: " + checkout.size);
+            System.out.println("Chips: " + checkout.chips);
+            System.out.println("Drink: " + checkout.drink);
+            System.out.println("Total Price: $" + totalPriceFromCheckout);
+
+            // Save the receipt to a file
+            try {
+                File receipt = new File("20230530-121523.txt");
+                FileWriter writer = new FileWriter(receipt);
+                writer.write("Order Details:\n");
+                writer.write("Bread: " + checkout.bread + "\n");
+                writer.write("Size: " + checkout.size + "\n");
+                writer.write("Chips: " + checkout.chips + "\n");
+                writer.write("Drink: " + checkout.drink + "\n");
+                writer.write("Total Price: $" + totalPriceFromCheckout + "\n");
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("An error occurred while creating the receipt.");
+                e.printStackTrace();
+            }
+
+        } else if (optionChoice == 2) {
+            System.out.println("Order canceled. Going back to the home screen...");
+
+        } else {
+            System.out.println("Invalid option selected. Going back to the home screen...");
+
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 
     private static void drinkSize() {
